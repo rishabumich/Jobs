2bee4491e36c8772802995bc0ac66d2aefc7545b
Rishab Gupta <rishabg@umich.edu>
2023-11-12 17:31:00 -0500

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,22 @@
+import {View, ScrollView, SafeAreaView, Text} from 'react-native';
+import {Stack, useRouter} from 'expo-router';
+
+import {COLORS, icons, images, SIZSES} from '../constants';
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components'
+
+const Home = () => {
+
+    const router = useRouter();
+    return (
+        <SafeAreaView style={ {flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen
+            options = {{
+                headerStyle : {backgroundColor: COLORS.lightWhite}
+            }}
+            />
+        </SafeAreaView>
+    )
+}
+
+export default Home;
+
